dbca4d18c4b7067b812f296a013c66e6fe1f1c37
Surendhar <surendharyaswa123@gmail.com>
2023-02-09 10:05:27 +0530

    first commit

---
+++ new/index.cs
@@ -0,0 +1,18 @@
+class Mobile
+{
+    public string Brand {get;set;}
+    public string Model {get;set;}
+    public int Price {get;set;}
+
+    public int Ram {get;set;}
+    public int Storage {get;set;}
+
+    static void Main(string[] args)
+    {
+        Mobile obj=new obj();
+        
+
+
+    }
+
+}
